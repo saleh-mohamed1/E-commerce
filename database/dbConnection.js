741e89b4df21164c7cf31c34835edb0aa847a907
saleh-mohamed1 <saleh.mohamd203@gmail.com>
2024-08-17 12:07:28 +0300

    git init

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 
-export const dbConn =mongoose.connect(process.env.SEC_DATA_BASE).then(()=>{
+export const dbConn =mongoose.connect('mongodb+srv://e-comm-final:MqMP6eFojilA3UKc@cluster0.255tp.mongodb.net/E-Commerece-App').then(()=>{
     console.log('dataBase is Connecting Successfully.......');
 }).catch((err)=>{
     console.log('dataBase is Connecting wrongly.......',err);
