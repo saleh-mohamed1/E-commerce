6c9256b913cd3a68bdf6b2df411a0d2b2bab101a
saleh-mohamed1 <saleh.mohamd203@gmail.com>
2024-08-17 15:55:16 +0300

    Paid Stripe

--- index.js
+++ index.js
@@ -7,20 +7,73 @@ import { AppErorr } from './src/middleware/AppErorr.js'
 import 'dotenv/config';
 import { catchErorr } from './src/middleware/catchErorr.js'
 import Stripe from 'stripe'
+import { Cart } from './database/model/cart.model.js'
+import { Order } from './database/model/order.model.js'
+import { User } from './database/model/user.model.js'
+import { Product } from './database/model/products.model.js'
 const stripe = new Stripe('sk_test_51Po40gJv2mEJYEHUXheUsVPsAVcRQLm9RZ07BAt3VxxT9Qi5nuKiPD995zR4qsz2sLjMm6NWl6Y97VbrKmrFEyfu00RfVDLq5R');
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.post('/api/webhook', express.raw({type: 'application/json'}), catchErorr((req, res) => {
+app.post('/api/webhook', express.raw({type: 'application/json'}), catchErorr(async(req, res,next) => {
     const sig = req.headers['stripe-signature'].toString()
   
     let event = stripe.webhooks.constructEvent(req.body, sig, 'whsec_02hBBRi2wdrze0FhOVB28rFRiDyJjnx6');
     
-    let checkoutSessionCompleted 
+    let checkOutPay 
     if (event.type == 'checkout.session.completed') {
-        
-        checkoutSessionCompleted = event.data.object;
+        checkOutPay = event.data.object;
+        // ^get User cart by cart Id
+        // !find User to get email
+        let currentUser = await User.findOne({email:checkOutPay.customer_email})
+         let cart = await Cart.findById(checkOutPay.client_reference_id)
+         if(!cart) return next(new AppErorr('cart Not Found'),404)
+             // ^total Order price
+         let totalOrderPriceToPay = cart.totalCartPriceAfterDiscount || cart.totalCartPrice
+         // ^Crate Order 
+         let order = new Order({
+             user: currentUser._id,
+             orderItems:cart.cartItems,
+             shippingAdress:checkOutPay.metadata,
+             totalOrderPriceToPay:checkOutPay.amount_total / 100,
+             paymentType:'Card',
+             isPaid:true
+         })
+         
+         await order.save()
+         
+         // ^increament and decrement
+         let proccessToUpdateStockAndSold = cart.cartItems.map((product)=>{
+             return ( {updateOne:{
+                 "filter":{ _id :product.product  },
+                 "update":{$inc:{ sold:product.quantity , stock:-product.quantity   }}    
+             }})
+         })
+         await Product.bulkWrite(proccessToUpdateStockAndSold) 
+         // ^clear user cart
+         await Cart.findByIdAndDelete(cart._id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
   
   
